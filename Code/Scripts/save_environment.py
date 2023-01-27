28cff9a99281411e81d8fb373cd726cbde097549
WillemsBrecht <brecht.willems2@student.howest.be>
2023-01-27 15:47:25 +0100

    Update a

--- Code/Scripts/save_environment.py
+++ Code/Scripts/save_environment.py
@@ -12,7 +12,7 @@ def save_conda_environment(root_of_project):
     print_messages("Started saving the environment to a YAML file...")
     location_of_file = os.path.join(root_of_project, "environment_file\\env.yaml")
     print(location_of_file)
-    subprocess.run(f'conda activate postgrad && conda env export -f { location_of_file } && conda deactivate', shell=True)
+    subprocess.run(f'conda activate postgrad && conda env export -f { location_of_file } && conda deactivate', shell=False)
     print_messages(f"Environment file saved to location { location_of_file }")
 
 
